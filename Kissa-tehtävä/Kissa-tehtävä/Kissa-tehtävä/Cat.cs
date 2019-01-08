5dc13b611ca7304b84141be4f34c1f250de1f5da
Lumi Liikanen <lumi_liikanen@hotmail.com>
2019-01-08 15:10:29 +0200

    Program Kissa-tehtävä Done

---
+++ Kissa-tehtävä/Kissa-tehtävä/Kissa-tehtävä/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kissa_tehtävä
+{
+    class Cat
+    {
+        public string name;
+        public int weight;
+
+        public Cat(string name, int weightValue)
+        {
+            this.name = name;
+            this.weight = weightValue;
+            Console.WriteLine($"Luotu kissa {this.name}");
+        }
+        public Cat(int weightValue)
+        {
+            this.weight = weightValue;
+        }
+        public void increaseweight(int value)
+        {
+            this.weight += value;
+        }
+        public void PrintWeight()
+        {
+            Console.WriteLine($"Kissan {this.name} uusi paino on {this.weight}");
+        }
+    }
+}
---
+++ Kissa-tehtävä/Kissa-tehtävä/Kissa-tehtävä/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Kissa_tehtävä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kissaohjelma");
+            Cat MyFirstCat = new Cat("Heiskanen", 1);
+            MyFirstCat.increaseweight(1);
+            MyFirstCat.PrintWeight();
+        }
+    }
+}
