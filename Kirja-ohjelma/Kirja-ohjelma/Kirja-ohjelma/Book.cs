46c664941c59fa7af48761bb3e352b64a0294000
Lumi Liikanen <lumi_liikanen@hotmail.com>
2019-01-11 13:15:53 +0200

    Program Kirja-ohjelma is done!

---
+++ Kirja-ohjelma/Kirja-ohjelma/Kirja-ohjelma/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Kirja_ohjelma
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string id;
+        public decimal price;
+
+        public Book(string title, string author, string id, decimal price)
+        {
+            this.title = title;
+            this.author = author;
+            this.id = id;
+            this.price = price;
+            Console.WriteLine($"Kirjan nimi on {this.title}.\nKirjan on kirjoittanut {this.author}.\nKirjan id-numero {this.id}.\nKirjan hinta {this.price.ToString("c",CultureInfo.CurrentCulture)}.");
+        }
+        public string Compare(Book my2ndBook)
+        {
+            string text = string.Empty;
+            if (this.price > my2ndBook.price)
+                text = $"{this.title} on kalliimpi kuin {my2ndBook.title}";
+            else
+                text = $"{my2ndBook.title} on kalliimpi kuin {this.title}";
+            return text;
+        }
+    }
+}
---
+++ Kirja-ohjelma/Kirja-ohjelma/Kirja-ohjelma/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kirja_ohjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirja ohjelma!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Book MyBook = new Book("Mahtava", "Lumi Liikanen", "00101", 20.00M);
+            Book my2ndBook = new Book("Mahtava2", "Lumi Liikanen", "00102", 28.00M);
+
+            Console.WriteLine(MyBook.Compare(my2ndBook));
+        }
+    }
+}
